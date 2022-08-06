d66277daed0428e77075f13b10ba86042286e826
K-M0613 <yishumenma245@gmail.com>
2022-08-06 02:42:34 +0000

    ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,41 @@
 Rails.application.routes.draw do
+   devise_for :admins, skip: [:registrations, :passwords], controllers: {
+     sessions: "admin/sessions"
+   }
+   
+   get 'users/edit' => 'public/users#edit', as: 'edit_user'
+   get 'users' => 'public/users#update', as: 'update_user'
+   
+   devise_for :users, skip: [:passwords], controllers: {
+     registrations: "public/registrations",
+     sessions: 'public/sessions'
+   }
+   
+   root to: "public/homes#top"
+   get "about" => "public/homes#about", as: "about"
+   
+   resources :posts, only[:new, :index, :search, :create, :destroy]
+   
+   get 'users/unsubscribe' => 'public/users#unsubscribe', as: 'unsubscribe'
+   patch 'users/withdraw' => 'public/users#withdraw', as: 'withdraw'
+   get 'users/my_page' => 'public/users#show', as: 'my_page'
+   
+   resources :games, only[:search, :show]
+   
+   resources :book_marks, only[:index, :create, :destroy]
+   
   namespace :admin do
-    get 'users/index'
-    get 'users/show'
-    get 'users/unsubscribe'
+    root to: "admin/homes#top"
+    get 'users/index' => 'users#index', as: 'users'
+    get 'users/show' => 'users#show', as: 'user'
+    get 'users/unsubscribe' => 'users#unsubscribe', as: 'unsubscribe'
+    patch 'users/withdraw' => 'users#withdraw', as: 'withdraw'
+    resources :posts, only[:index, :show, :destroy]
   end
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'book_marks/index'
-  end
-  namespace :public do
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/search'
-  end
-  namespace :public do
-    get 'users/show'
-    get 'users/edit'
-    get 'users/unsubscribe'
-  end
-  namespace :public do
-    get 'games/search'
-    get 'games/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  devise_for :admins
-  devise_for :users
+  
+  
+  
+  
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
