5dd2a9e0cbd7d1b5721cfd49cfd736daabc10a28
K-M0613 <yishumenma245@gmail.com>
2022-08-22 17:00:17 +0000

    ログイン・登録画面レイアウト

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -10,7 +10,7 @@ class Public::SessionsController < Devise::SessionsController
    def guest_sign_in
     user = User.guest
     sign_in user
-    redirect_to my_page_path(current_user), notice: 'guestuserでログインしました。'
+    redirect_to root_path, notice: 'guestuserでログインしました。'
    end
 
   # GET /resource/sign_in
